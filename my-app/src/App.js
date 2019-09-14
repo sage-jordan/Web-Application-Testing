eaf83b60ec20014a1154d82e80f196b18ebc0d5b
sage-jordan <sagemjrodan@gmail.com>
2019-09-13 21:01:14 -0400

    App to class component, moved const's to state

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,38 @@
 import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-  const strikes = useState(0);
-  const balls = useState(0);
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      strikes: 0,
+      balls: 0
+    };
+    console.log("constructor");
+  }
 
-  return (
-    <div className="App">
-      <div className="balls">
-        <h1>Balls</h1>
-        <span className="box">{balls}</span>
+  componentDidMount() {
+    const hit = document.querySelector('.hit');
+    const foul = document.querySelector('.foul');
+    console.log("component did mount", hit, foul);
+  };
+
+  render() {
+    console.log("app render");
+    return (
+      <div className="App">
+        <div className="balls">
+          <h1>Balls</h1>
+          <span className="box">{this.state.balls}</span>
+        </div>
+        <div className="strikes"> 
+          <h1>Strikes</h1>
+          <span className="box">{this.state.strikes}</span>
+        </div>
+        <button className="hit">Hit</button>
+        <button className="foul">Foul</button>
       </div>
-      <div className="strikes"> 
-        <h1>Strikes</h1>
-        <span className="box">{strikes}</span>
-      </div>
-      <button className="hit">Hit</button>
-      <button className="foul">Foul</button>
-    </div>
-  );
+  )}; 
 }
 
 export default App;
