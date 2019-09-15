66dcb5b27f2ac2b5a1bb0c4f5b95a844fb64a6cb
sage-jordan <sagemjrodan@gmail.com>
2019-09-14 22:58:20 -0400

    added test file helpers.js

---
+++ my-app/src/helpers.js
@@ -0,0 +1,8 @@
+const help = require("./Dashboard");
+describe('dashboard module', () => {
+    describe('componentDidUpdate=>reset balls/strikes when strikes=0 or balls=4', () => {
+        it('when strikes=0 or balls=4', () => {
+            const expectedOutput = 
+        })
+    })
+})
